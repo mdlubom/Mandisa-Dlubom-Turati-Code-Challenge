dfe5520eac07e7abec197d764da1a89e3a42808b
Mandisa <mandisa@responsive.co.za>
2020-08-26 16:10:19 +0200

    updated code

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,76 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Mandisa Dlubom Turati code challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <header className={styles.header}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            Mandisa <a href="https://www.turati.co.za/" target="_blank">Turati</a> code challenge
+          </h1>
+      </header>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <main className={styles.main}>
+        <div className={styles.grid.about}>
+          <h2>Create a single page web application with the following:</h2>
+          
+          <ol>
+            <li>A youtube integrated video in the centre of the page</li>
+            <li>Under the video it a contact form with fields for
+                <ul>
+                  <li>Name</li>
+                  <li>Email</li>
+                  <li>Message (The contact form must send an email to yourself with the details of the contact form)</li>
+                </ul>
+            </li>
+          </ol>
+        </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <h2>Follow-up questions:</h2>
+          
+          <ol>
+            <li>What did you accomplish:
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                <ul>
+                  <li>
+                    I managed to create the single page web application with a youtube video and uploaded it to github <a href="https://github.com/mdlubom/Mandisa-Dlubom-Turati-Code-Challenge" target="_blank">Mandisa Turati Code Challenge</a>
+                  </li>
+                </ul>
+            </li>
+            <li>What did you learn:
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              <ul>
+                <li>
+                  From this challenge I learnt that before jumping into a new project it is key to first take time to learn your required tools or frameworks. It is great to know how to translate design or project requirements in the planning but there also needs top be understanding of the language that will be used to create a successful project.
+                </li>
+              </ul>
+            </li>
+          </ol>
         </div>
+
+        <iframe className={styles.iframe} src="https://www.youtube.com/embed/LkSgxuQrYwY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        {/* <div className={styles.formWrapper}>
+          <form action="#">
+            <label for="fname">First name:</label>
+            <input type="text" name="fname" placeholder="Your name"><br/>
+            <label for="lname">Last name:</label><br/>
+            <input type="text" name="lname" placeholder="Your surname"><br><br>
+            <input type="submit" value="Submit"/>
+          </form> 
+        </div> */}
+
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>Mandisa Dlubom Turati code challenge</p>
       </footer>
     </div>
   )
